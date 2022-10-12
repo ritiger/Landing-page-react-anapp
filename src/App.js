fd8f60ddc19847f6b7ec01c74b291e49d148bba7
Ri Tiger <90784203+ritiger@users.noreply.github.com>
2022-10-12 16:09:48 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./assets/styles.css";
+import Navbar from "./components/Navbar";
+
+import HeroBanner from "./components/HeroBanner";
+import Features from "./components/Features";
+import Services from "./components/Services";
+import Screenshots from "./components/Screenshots";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
+import Download from "./components/Download";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Features />
+        <Services />
+        <Screenshots />
+        <Pricing />
+        <Download />
+      </main>
+      <Footer />
+    </>
+  );
+}
